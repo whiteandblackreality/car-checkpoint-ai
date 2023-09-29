8f1f1245c2b01c6b4ac88e393cd3a922b283ec58
n.moroshkin <n.moroshkin@cism-ms.ru>
2023-09-29 16:08:17 +0300

    add base repository for db-repository

---
+++ db_repository/app/repositories/__init__.py
@@ -0,0 +1,25 @@
+from abc import abstractmethod
+from typing import Generic, List, TypeVar
+
+M = TypeVar("M")
+
+K = TypeVar("K")
+
+
+class RepositoryMeta(Generic[M, K]):
+
+    @abstractmethod
+    def create(self, instance: M) -> M:
+        pass
+
+    @abstractmethod
+    def delete(self, id: K) -> None:
+        pass
+
+    @abstractmethod
+    def get(self, id: K) -> M:
+        pass
+
+    @abstractmethod
+    def list(self, limit: int, start: int) -> List[M]:
+        pass
