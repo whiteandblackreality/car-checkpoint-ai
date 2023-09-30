6dc836778f54d13ce11df7543e9ae7b51716c0f5
n.moroshkin <n.moroshkin@cism-ms.ru>
2023-09-30 11:52:49 +0300

    add metadata for routers for db-repository

---
+++ db_repository/app/metadata/tags.py
@@ -0,0 +1,14 @@
+Tags = [
+    {
+        "name": "cars",
+        "description": "Contains CRUD operation on Car entity",
+    },
+    {
+        "name": "entries",
+        "description": "Contains CRUD operation on Entry entity",
+    },
+    {
+        "name": "cameras",
+        "description": "Contains CRUD operation on Camera entity",
+    },
+]
