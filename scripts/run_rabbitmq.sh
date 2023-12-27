df7660940c8102e5155432601428dce64302fd86
whiteandblackreality <whiteandblackreality@hmail.com>
2023-12-27 17:28:23 +0300

    update script for running rabbitmq

--- scripts/run_rabbitmq.sh
+++ scripts/run_rabbitmq.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-sudo docker run --rm -p 15672:15672 rabbitmq:3.10.7-management # quest:quest
+sudo rm crid
+sudo docker run -d --rm -p 5672:5672 -p 15672:15672 rabbitmq:3.10.7-management >> crid # quest:quest
+echo "RabbitMQ is worked"
